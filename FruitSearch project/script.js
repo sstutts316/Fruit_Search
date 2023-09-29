583f19989dabb7d605d3ab5931216caa1b791ef6
Steven Stutts <86498128+sstutts316@users.noreply.github.com>
2023-09-29 16:36:51 -0500

    Update script.js

--- FruitSearch project/script.js
+++ FruitSearch project/script.js
@@ -3,34 +3,21 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-// Function to populate the list items based on user input
-function populateSuggestions(inputVal) {
-    inputVal = inputVal.toLowerCase(); // Convert input to lowercase for case-insensitive matching
-    let results = [];
-
-    for (let fruitName of fruit) {
-        if (fruitName.toLowerCase().includes(inputVal)) {
-            results.push(fruitName);
-        }
-    }
-
-    showSuggestions(results, inputVal);
-}
-
-
 function search(inputVal) {
     inputVal = inputVal.toLowerCase(); // Convert input to lowercase for case-insensitive matching
     let results = [];
 
-    for (let fruitName of fruit) {
-        if (fruitName.toLowerCase().includes(inputVal)) {
-            results.push(fruitName);
-        }
-    }
+    // use a filter function to filter search results
+    results = fruit.filter(fruitName => fruitName.toLowerCase().includes(inputVal));
+
+    // for (let fruitName of fruit) {
+    //     if (fruitName.toLowerCase().includes(inputVal)) {
+    //         results.push(fruitName);
+    //     }
+    // }
 
     showSuggestions(results, inputVal); // Automatically show suggestions
 }
-
 
 function showSuggestions(results, inputVal) {
     const ul = document.querySelector('.suggestions ul');
@@ -43,8 +30,10 @@ function showSuggestions(results, inputVal) {
     }
 
     if (results.length > 0) {
+        document.querySelector('.suggestions').style.display = 'block';
         ul.classList.add('has-suggestions');
     } else {
+        document.querySelector('.suggestions').style.display = 'none';
         ul.classList.remove('has-suggestions');
     }
 }
@@ -53,12 +42,20 @@ function useSuggestion(e) {
     if (e.target.tagName === 'LI') {
         input.value = e.target.textContent;
         suggestions.innerHTML = ''; // Clear the suggestions dropdown
+        document.querySelector('.suggestions').style.display = 'none';
     }
 }
 
 input.addEventListener('input', function () {
     const inputVal = input.value;
-    search(inputVal);
+    if (inputVal.length > 0) {
+        search(inputVal);
+    }
+    else {
+        document.querySelector('.suggestions').style.display = 'none';
+        suggestions.innerHTML = ''; // Clear the suggestions dropdown
+    }
+    
 });
 
 suggestions.addEventListener('click', useSuggestion);
